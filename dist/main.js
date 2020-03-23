1c52e7c64423af4d2360fd8805d79be0d37ee0e7
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2020-03-23 14:19:33 -0300

    removed node_modules
    Updated restify version

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,6 @@
-import * as restify from 'restify';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const restify = require("restify");
 const server = restify.createServer({
     name: 'meat-api',
     version: '1.0.0'
@@ -8,5 +10,5 @@ server.get('/hello', (req, resp, next) => {
     return next();
 });
 server.listen(3000, () => {
-    console.log('API ESTA RODANDO CARALHO');
+    console.log('API is running on http://localhost:3000');
 });
